c815220b8ae8d1a948abc79dc88d86bf21e3d763
louisyuzhe <louisyuzhe@gmail.com>
2020-11-07 20:28:39 -0600

    Import and test all 3 modules on main class

--- main.py
+++ main.py
@@ -4,20 +4,61 @@ Created on Wed Nov  4 23:51:34 2020
 
 @author: Yu Zhe
 """
-from forecast_arima import Predictor_arima
+from forecast_arima import Predictor_ARIMA
 from forecast_prophet import Predictor_fbprophet
+from forecast_arma import Predictor_ARMA
 
 import pandas as pd
-
 df = pd.read_csv("../monthly-car-sales.csv")
-test = Predictor_arima(df)
-test.fit_model()
-test.evaluate_model(output=False)
-test.analyze_estimator(output=False)
-pred_result = test.outsample_forecast(output=False)
-print(pred_result)
-test.diagnostic_model()
+
 #%%
-test2 = Predictor_fbprophet(df)
-print(test2.outsample_forecast(output=False))
-print(test2.evaluate_model(output=False))
+"""
+ARIMA
+"""
+arima_result = Predictor_ARIMA(df)
+arima_result.fit_model()
+arima_result.evaluate_model(output=False)
+arima_result.analyze_estimator(output=False)
+arima_pred = arima_result.outsample_forecast(output=False)
+print(arima_pred)
+#arima_pred.plot()
+arima_result.diagnostic_model()
+
+#%%
+"""
+Fbprophet
+"""
+fbprophet_result = Predictor_fbprophet(df)
+fbprophet_pred = fbprophet_result.outsample_forecast(output=False)
+print(fbprophet_pred)
+print(fbprophet_result.evaluate_model(output=False))
+fbprophet_pred_yhat = fbprophet_pred['yhat']
+fbprophet_pred_yhat.index = fbprophet_pred['ds']
+
+#%%
+
+arma_result = Predictor_ARMA(df)
+arma_result.evaluate_model(output=False)
+print(eval_result)
+arma_pred = arma_result.outsample_forecast(output=False)
+#arma_pred.plot()
+arma_pred+=fbprophet_seasonality
+#%%
+"""
+Comparison
+"""
+
+original_df = df.set_index('ds')
+ax = original_df['y'].plot(label= 'Observed', color='cyan', legend=True)
+
+arima_pred.plot(label= 'ARIMA_forecast', color='blue', legend=True, ax=ax)
+fbprophet_pred_yhat.plot(label= 'fbprophet_forecast', color='red', legend=True, ax=ax)
+arma_pred.plot(label= 'ARMA_forecast', color='green', legend=True, ax=ax)
+
+#%%
+"""
+Get Seasonality
+"""
+fbprophet_seasonality = fbprophet_pred['additive_terms']
+fbprophet_seasonality.index = fbprophet_pred['ds']
+fbprophet_seasonality.plot()
